d341833e96deb3d8e59234da85ce077e27def089
bigboom_co <1039648410@qq.com>
2019-12-09 22:54:19 +0800

    002

--- app/src/main/java/android/bignerdranch/photopreview/ImageDelegate.java
+++ app/src/main/java/android/bignerdranch/photopreview/ImageDelegate.java
@@ -1,8 +1,124 @@
 package android.bignerdranch.photopreview;
 
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.graphics.Rect;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.os.Message;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.animation.DecelerateInterpolator;
 
 public class ImageDelegate {
+    private static final String TAG = "ImageDelegate";
+
+    enum ScaleType {
+
+        FIT_AUTO(1),
+
+        FIT_CENTER(2),
+
+        CENTER(3),
+
+        CENTER_INSIDE(4);
+
+        ScaleType(int ni) {
+            defaultType = ni;
+        }
+         int defaultType;
+    }
+
     //获取屏幕宽高
     private DisplayMetrics mDisplayMetrics;
+    private Callback mCallback;
+    private PhotoPreviewHandler mHandler;
+    private ScaleType mScaleType = ScaleType.FIT_CENTER;
+
+    public ImageDelegate(DisplayMetrics metrics, Callback callback) {
+        mDisplayMetrics = metrics;
+        mCallback = callback;
+        HandlerThread handlerThread = new HandlerThread(TAG);
+        handlerThread.start();
+        mHandler = new PhotoPreviewHandler(handlerThread.getLooper());
+
+
+    }
+
+    public void setScaleType(ScaleType scaleType) {
+        mScaleType = scaleType;
+        Log.e(TAG, "set ScaleType: "+mScaleType);
+
+        if (mState.ordinal() >= State.INIT.ordinal()) {
+            mState = State.INIT;
+            mImage.mCurrentState = null;
+            requestInvalidate();
+        }
+    }
+
+
+
+    //Delegate会调用view的这些函数
+    public interface Callback {
+        void onRequestInvalidate();
+
+        boolean onRequestAwakenScrollBars();
+
+        void onScaleChange(float scale);
+    }
+
+    private class PhotoPreviewHandler extends Handler {
+
+        public PhotoPreviewHandler(Looper looper) {
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_IMAGE_DRAW:
+                    prepareDraw((Rect) msg.obj);
+                    requestInvalidate();
+                    break;
+
+                case MSG_IMAGE_SCALE:
+                    initScaleType((Rect) msg.obj);
+                    requestInvalidate();
+                    requestAwakenScrollBars();
+                    break;
+
+                case MSG_IMAGE_SRC:
+                    prepare((ImageDecoder) msg.obj);
+                    requestInvalidate();
+                    break;
+
+                case MSG_IMAGE_LOAD:
+                    load();
+                    requestInvalidate();
+                    break;
+
+                case MSG_IMAGE_INIT:
+                    initialize((Rect) msg.obj);
+                    requestInvalidate();
+                    break;
+
+                case MSG_IMAGE_RELEASE:
+                    release();
+                    break;
+
+                case MSG_QUIT:
+                    release();
+                    try {
+                        getLooper().quit();
+                    } catch (Throwable throwable) {
+                        Logger.w(TAG, throwable);
+                    }
+                    break;
+            }
+        }
+
 }
+
+
+
--- app/src/main/java/android/bignerdranch/photopreview/PhotoPreview.java
+++ app/src/main/java/android/bignerdranch/photopreview/PhotoPreview.java
@@ -8,7 +8,7 @@ import android.view.View;
 
 import androidx.annotation.Nullable;
 
-public class PhotoPreview extends View {
+public class PhotoPreview extends View implements ImageDelegate.Callback {
     private static final String TAG = "PhotoPreview";
 
     ImageDelegate mImageDelegate ;
@@ -29,5 +29,38 @@ public class PhotoPreview extends View {
 
     private void initialize(Context context, AttributeSet attrs, int defStyleAttr){
         mImageDelegate = new ImageDelegate(getResources().getDisplayMetrics(), this);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.IntensifyImageView);
+
+
+        Log.e(TAG,"initialize before ScaleType");
+        mImageDelegate.setScaleType(ScaleType.valueOf(
+                a.getInt(R.styleable.IntensifyImageView_scaleType, ScaleType.FIT_CENTER.nativeInt)));
+        mImageDelegate.setMinimumScale(
+                a.getFloat(R.styleable.IntensifyImageView_minimumScale, 0f));
+
+        mImageDelegate.setMaximumScale(
+                a.getFloat(R.styleable.IntensifyImageView_maximumScale, Float.MAX_VALUE));
+
+        mImageDelegate.setScale(a.getFloat(R.styleable.IntensifyImageView_scale, -1f));
+
+        a.recycle();
+    }
+
+
+
+    //Delegate调用view的这些函数
+    @Override
+    public void onRequestInvalidate() {
+
+    }
+
+    @Override
+    public boolean onRequestAwakenScrollBars() {
+        return false;
+    }
+
+    @Override
+    public void onScaleChange(float scale) {
+
     }
 }
